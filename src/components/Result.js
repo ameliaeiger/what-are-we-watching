60aa6df3107d82dbb7511b961832477106d81dc7
Nathan Hodnett <nathanhodnett@gmail.com>
2022-08-28 10:54:49 -0600

    Add Result component, and add initial logic and styling to Results view

---
+++ src/components/Result.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react"
+import { View, Text, Image, StyleSheet } from "react-native"
+
+const Result = () => {
+    // const [result, setResult] = useState({})
+    return(
+        <View
+            style={styles.resultContainer}>
+            <View>
+               <Text
+                    style={styles.titleText}>
+                Congratulations! Your party chose...
+               </Text>
+               <Image
+                    style={styles.imageResult}>
+                
+               </Image>
+               <Text
+                    style={styles.resultText}>
+                {/* SHREK 2!!! */}
+               </Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    resultContainer: {
+        display:"flex",
+        alignItems:"center",
+        justifyContent:"center",
+        width:"100%",
+        height:"80%",
+        backgroundColor:"#f4f1f1"
+    },
+    titleText: {
+        fontFamily:"courier",
+        fontSize:"20px",
+        color:"#F37180"
+    },
+    imageResult: {
+        width: 200, 
+        height: 300
+    },
+    resultText: {
+        fontFamily:"courier",
+        fontSize:"60px",
+        color:"#F37180"
+    }
+})
+
+
+export default Result
---
+++ src/views/ResultsView.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Header from "../components/Header.js"
+
+
+const ResultsView = () => {
+
+    return(
+        <>
+        <Header />
+        </>
+    )
+}
+
+export default ResultsView
