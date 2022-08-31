061c7b31726d3d747a871b36d4be9a20d49563c5
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-08-31 02:16:42 -0600

    Hero image

--- src/views/VotingView.js
+++ src/views/VotingView.js
@@ -1,5 +1,6 @@
 //IMPORTS
 import React, { useEffect, useState } from "react"
+import { TouchableOpacity, Text, StyleSheet } from "react-native"
 
 //COMPONENTS
 import HeroImage from "../components/HeroImage.js"
@@ -8,22 +9,58 @@ import Loading from "../components/Loading.js"
 //DATA
 import movieData from "../../moviedata.js"
 
-const VotingView = ( {navigation} ) => {
-    const [allData, setAllData] = useState("")
+const VotingView = ({ navigation }) => {
+    const [movieChunk, setMovieChunk] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    const [currentMovie, setCurrentMovie] = useState(movieData.poster_path)
     const [loaded, setLoaded] = useState(false)
 
-    useEffect(() => {
-        setAllData(movieData)
-        setLoaded(true)
-    },[])
 
+// GENERATES AND RETURNS STRING CONTAINING URL TO ALL OF A SPECIFIED MOVIE'S DATA
+    const getPosterPath = (num) => {
+        return "https://api.themoviedb.org/3/movie/" + num.toString() + "?api_key=d68f62b1dd987551ff4793fc96f457f1"
+    }
+
+    const fetchImage = (url) => {
+        return fetch(url)
+            .then(response => response.json())
+            .then(data => {
+               console.log(data)
+               return data
+            })
+    }
+
+    useEffect(() => {
+            setLoaded(true)
+    },[currentMovie])
+
+    const loadImages = () => {
+        return (
+            <HeroImage
+            poster={currentMovie}
+            navigation={navigation}
+         />
+        )
+    }
+
+    console.log(movieData.poster_path)
     return(
         <>
         {loaded ? 
+            <>
             <HeroImage
-                poster={allData.poster_path}
+                poster={movieData.poster_path}
                 navigation={navigation}
-             /> :
+             />
+            <TouchableOpacity
+                style={styles.buttonNo}>
+                <Text>No</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.buttonYes}>
+                <Text>Yes</Text>
+            </TouchableOpacity>
+            </>
+             :
             <Loading />
         }
         </>
@@ -31,3 +68,32 @@ const VotingView = ( {navigation} ) => {
 }
 
 export default VotingView
+
+const styles = StyleSheet.create({
+    buttonNo: {
+        display:"flex",
+        alignItems:"center",
+        justifyContent:"center",
+        height:100,
+        width:100,
+        backgroundColor:"red",
+        opacity:.8,
+        position:"absolute",
+        bottom:15,
+        left:"15%",
+        borderRadius:100,
+    },
+    buttonYes: {
+        display:"flex",
+        alignItems:"center",
+        justifyContent:"center",
+        height:100,
+        width:100,
+        backgroundColor:"green",
+        opacity:.8,
+        position:"absolute",
+        bottom:15,
+        right:"15%",
+        borderRadius:100,
+    },
+})
