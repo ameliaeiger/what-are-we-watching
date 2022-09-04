7501db82a09210a1ca8e5726297c954ada5b13ba
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-09-04 11:29:12 -0600

    Implement GraphQL calls

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react"
 import { View, StyleSheet, Image, Dimensions, TouchableOpacity, Text } from "react-native"
 
+//COMPONENTS
+import VotingButton from "./VotingButton"
+
 const HeroImage = ({source, navigation, handleVotePress}) => {
     const windowWidth = Dimensions.get('window').width;
     const windowHeight = Dimensions.get('window').height;
@@ -25,18 +28,6 @@ const HeroImage = ({source, navigation, handleVotePress}) => {
                     source={source}
                      />
                 </TouchableOpacity>
-                <TouchableOpacity
-                title="no"
-                onPress={(e) => handleVotePress(e, false)}
-                style={styles.buttonNo}>
-                <Text>No</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                title="yes"
-                onPress={(e) => handleVotePress(e, true)}
-                style={styles.buttonYes}>
-                <Text>Yes</Text>
-            </TouchableOpacity>
         </View>
     )
 }
@@ -46,32 +37,6 @@ const styles = StyleSheet.create({
         display:"flex",
         alignItems:"center",
         width:"100%"
-    },
-    buttonNo: {
-        display:"flex",
-        alignItems:"center",
-        justifyContent:"center",
-        height:100,
-        width:100,
-        backgroundColor:"red",
-        opacity:.8,
-        position:"absolute",
-        bottom:15,
-        left:"15%",
-        borderRadius:100,
-    },
-    buttonYes: {
-        display:"flex",
-        alignItems:"center",
-        justifyContent:"center",
-        height:100,
-        width:100,
-        backgroundColor:"green",
-        opacity:.8,
-        position:"absolute",
-        bottom:15,
-        right:"15%",
-        borderRadius:100,
     },
 })
 
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,16 +1,7 @@
-import React, { useState, useContext } from "react"
+import React, { useState, useContext, useEffect } from "react"
 import { View, Text, TextInput, StyleSheet, TouchableOpacity, TouchableWithoutFeedback, Keyboard, useWindowDimensions, Alert } from "react-native"
-import { useMutation,  gql } from "@apollo/client"
+import { useMutation, gql } from "@apollo/client"
 import AppContext from "./AppContext"
-
-    const USER_LOGIN_CHECK = gql`
-    mutation createUser($userName: userName!) {
-        createUser(userName: $userName) {
-            userName
-            userId
-        }
-    }
-`
 
 const userData = [
     {
@@ -25,11 +16,24 @@ const userData = [
         "userName": "User3",
         "userId": "420244"
     },
+    {
+        "userName": "LOOK-AT-THIS",
+        "userId": "420699"
+    },
 ]
 
-// {
-//     "userName": "User2"
-//   }
+const USER_LOGIN_CHECK = gql`
+mutation createUser($input: CreateUserInput!){
+    createUser(input: $input) {
+        user {
+            id
+            name
+        }
+    }
+}`
+   
+
+
 
 const LoginForm = ({navigation}) => {
     const globals = useContext(AppContext);
@@ -37,28 +41,70 @@ const LoginForm = ({navigation}) => {
     const [userNameValue, setUserNameValue] = useState("")
     const [loginError, setLoginError] = useState(false)
 
-    const [userLogin, { data, loading, error }] = useMutation(USER_LOGIN_CHECK, { 
-        variables: {"userName": globals.userNameValue }, 
-        onCompleted: () => navigation.navigate("CreateEventView") 
+const [userLogin, { data, loading, error }] = useMutation(USER_LOGIN_CHECK, { 
+        variables: {"input": {"name": globals.userInfo }}, 
+        // onCompleted: () => navigation.navigate("CreateEventView") 
+        onCompleted: () => testInfo(data)
     })
 
-    const runLogin = (e) => {
-        console.log('initial login value', globals.setLoggedIn)
-    //  if (globals.setLoggedIn === false) {
-    // if (userData.find((user) => user.userName === userNameValue)) 
-        console.log('login trigger')
-        globals.setLoggedIn(true)
-        globals.setUserInfo(userNameValue)
-        userLogin()
-        console.log('falsey', globals)
-     //}
-       if (globals.setLoggedIn){
-            navigation.navigate("CreateEventView")
-        } else {
-            setLoginError(true)
-        }
+    const testInfo = (data) => {
+        console.log("POST SENT")
+        navigation.navigate("CreateEventView")
+        console.log("RESPONSE: ", data)
+        console.log("----------------")
     }
 
+    const runLogin = (e) => {
+        // let isUser = userData.find((user) => user.userName === userNameValue)
+        if (!userNameValue) {
+            return Alert.alert("Please enter a username")
+        }
+        if (userNameValue) {
+            console.log("TEXT INPUT FIELD VALUE: ", userNameValue)
+            globals.setUserInfo(userNameValue)
+            globals.setLoggedIn(true)
+            // userLogin()
+        }
+        // if (globals.setLoggedIn && userNameValue) {
+            // console.log("USER LOGIN TRIGGERED")
+        // }
+     //}
+    //    if (globals.setLoggedIn){
+    //         navigation.navigate("CreateEventView")
+    //     } else {
+    //         setLoginError(true)
+    //     }
+    }
+     
+            
+    //         {
+    //     name: 
+    //     }) {
+    //         user {
+    //         name
+    //         id
+    //         }
+    //         errors
+    //     }
+    // }
+    // `
+
+    useEffect(() => {
+        console.log("useEffect: --", userNameValue)
+    },[userNameValue])
+
+    useEffect(() => {
+        if (!globals.loggedIn){
+            console.log("Please log in.")
+        } else {
+            userLogin()
+            console.log("----------------")
+            console.log("USER LOGIN TRIGGERED")
+            console.log(`You are now logged in, ${globals.userInfo}`)
+        }
+    },[globals.loggedIn])
+
+  
     return(
             <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
                 <View
@@ -78,9 +124,9 @@ const LoginForm = ({navigation}) => {
                                 style={styles.buttonText}>
                                     Login</Text>
                         </TouchableOpacity>
-                        {loading && <Text>Logging you in...</Text>}
+                        {/* {loading && <Text>Logging you in...</Text>}
                         {loginError && <Text>User does not exist</Text>}
-                        {data && <Text>{data.CreateUser.userName}</Text>}
+                        {data && <Text>{data.CreateUser.userName}</Text>} */}
                     </View>
                 </View>
             </TouchableWithoutFeedback>
