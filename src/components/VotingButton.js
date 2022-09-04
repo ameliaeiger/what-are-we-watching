90b8e01225d2c5a8d1cc7ca7d9b05cfb9ddf074d
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-09-04 11:29:35 -0600

    Add voting button component

---
+++ src/components/VotingButton.js
@@ -0,0 +1,53 @@
+import React from "react"
+import { TouchableOpacity, StyleSheet, Text } from "react-native"
+
+const VotingButton = ({ handleVotePress, text, boolVal }) => {
+    return (
+        <>
+        <TouchableOpacity
+            title={text}
+            onPress={(e) => handleVotePress(e, boolVal)}
+            // style={styles.buttonNo}
+            >
+            <Text>{text}</Text>
+        </TouchableOpacity>
+        {/* <TouchableOpacity
+            title="yes"
+            onPress={(e) => handleVotePress(e, true)}
+            style={styles.buttonYes}>
+            <Text>Yes</Text>
+        </TouchableOpacity> */}
+        </>
+    )
+}
+
+export default VotingButton
+
+const styles = StyleSheet.create({
+    buttonNo: {
+        display:"flex",
+        alignItems:"center",
+        justifyContent:"center",
+        height:100,
+        width:100,
+        backgroundColor:"red",
+        opacity:.8,
+        position:"absolute",
+        bottom:15,
+        left:"15%",
+        borderRadius:100,
+    },
+    buttonYes: {
+        display:"flex",
+        alignItems:"center",
+        justifyContent:"center",
+        height:100,
+        width:100,
+        backgroundColor:"green",
+        opacity:.8,
+        position:"absolute",
+        bottom:15,
+        right:"15%",
+        borderRadius:100,
+    },
+})
